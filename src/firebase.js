9ebdea7cf0495242026fb81aff6392c07df768d8
Melkisedek Ubalde <melkisedek222@gmail.com>
2020-10-18 18:41:37 +0800

    Creating Header Component

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAxK2jrN3AqMouki_fM1BLwgJ6UWko_uMY",
+	authDomain: "clone-88c7e.firebaseapp.com",
+	databaseURL: "https://clone-88c7e.firebaseio.com",
+	projectId: "clone-88c7e",
+	storageBucket: "clone-88c7e.appspot.com",
+	messagingSenderId: "149501415634",
+	appId: "1:149501415634:web:ea3266b0896c5f2f819d0b",
+};
